887a1546ff029503edb7443a5f55918f8225b083
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2022-06-22 15:56:23 +0200

    push

--- Hololens/Assets/Scripts/SiatSK20.cs
+++ Hololens/Assets/Scripts/SiatSK20.cs
@@ -9,19 +9,20 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.UI
     public class SiatSK20 : MonoBehaviour
     {
         public Animator MachineAnimations;
-        public bool pressed = false;
-
+       
         public float headHeight;
         public float alignment;
         public float alignmentKnopTurn;
 
-        public bool powerSwitch;
-      
+       // public bool powerSwitch = false;
+        public bool pressed = false;
+        public bool smallBoxTrigger = false;
+        public bool mediumBoxTrigger = false;
+        public bool largeBoxTrigger = false;
+
         public GameObject headSlider;
         public GameObject alignmentSlider;
-
         public GameObject handCranck;
-
         public GameObject boneAnim;
 
         private void Start()
@@ -69,78 +70,86 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.UI
             }
         }
 
-        public void PowerSwitchButton()
-        {
-            if (!powerSwitch)
-            {
-                MachineAnimations.SetFloat("Power Switch", 1f);
-                powerSwitch = true;
-            }
+        //public void PowerSwitchButton()
+        //{
+        //    if (!powerSwitch)
+        //    {
+        //        MachineAnimations.SetFloat("Power Switch", 1f);
+        //        powerSwitch = true;
+        //    }
 
-            if (powerSwitch)
-            {
-                MachineAnimations.SetFloat("Power Switch", 0f);
-                powerSwitch = false;
-            }
-        }
+        //    if (powerSwitch)
+        //    {
+        //        MachineAnimations.SetFloat("Power Switch", 0f);
+        //        powerSwitch = false;
+        //    }
+        //}
 
         public IEnumerator WaitForBox()
         {
             yield return new WaitForSeconds(1f);
+            smallBoxTrigger = false;
+            mediumBoxTrigger = false;
+            largeBoxTrigger = false;
 
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.transform.CompareTag("SmallBox") /*&& powerSwitch*/)
+            if (collision.transform.CompareTag("SmallBox")   /* powerSwitch */)
             {
                 Debug.Log("SmallBox Yes");
 
-                if (headHeight == 0.5f && alignment == 0.5f)
+                if (headHeight == 0.5f && alignment == 0.5f && !smallBoxTrigger)
                 {
-                collision.gameObject.transform.SetParent(boneAnim.transform);
-                MachineAnimations.SetTrigger("Tape Wheel Front Down");
-                MachineAnimations.SetTrigger("Tape Wheel Back Down");
-               
-                MachineAnimations.SetTrigger("Doos door machine");
-                StartCoroutine(WaitForBox());
-
-                //MachineAnimations.SetTrigger("Tape Wheel Front Up");
-                //MachineAnimations.SetTrigger("Tape Wheel Back Up");
-                // animatie doos door machine
-                }
-            }
-
-            if (collision.transform.CompareTag("MediumBox") && powerSwitch)
-            {
-                Debug.Log("MediumBox Yes");
-                if (headHeight == 0.75f && alignment == 0.75f)
-                {
+                    smallBoxTrigger = true;
                     collision.gameObject.transform.SetParent(boneAnim.transform);
-                    MachineAnimations.SetTrigger("Doos door machine");
-
                     MachineAnimations.SetTrigger("Tape Wheel Front Down");
                     MachineAnimations.SetTrigger("Tape Wheel Back Down");
+
+                    MachineAnimations.SetTrigger("Doos door machine");
                     StartCoroutine(WaitForBox());
-                    MachineAnimations.SetTrigger("Tape Wheel Front Up");
-                    MachineAnimations.SetTrigger("Tape Wheel Back Up");
+
+                    //MachineAnimations.SetTrigger("Tape Wheel Front Up");
+                    //MachineAnimations.SetTrigger("Tape Wheel Back Up");
                     // animatie doos door machine
                 }
             }
 
-            if (collision.transform.CompareTag("LargeBox") && powerSwitch)
+            if (collision.transform.CompareTag("MediumBox")  /* powerSwitch */)
             {
-                Debug.Log("LargeBox Yes");
-                if (headHeight == 1f && alignment == 1f)
+                Debug.Log("MediumBox Yes");
+                if (headHeight == 0.75f && alignment == 0.75f && !mediumBoxTrigger)
                 {
+                    mediumBoxTrigger = true;
                     collision.gameObject.transform.SetParent(boneAnim.transform);
                     MachineAnimations.SetTrigger("Doos door machine");
 
                     MachineAnimations.SetTrigger("Tape Wheel Front Down");
                     MachineAnimations.SetTrigger("Tape Wheel Back Down");
                     StartCoroutine(WaitForBox());
-                    MachineAnimations.SetTrigger("Tape Wheel Front Up");
-                    MachineAnimations.SetTrigger("Tape Wheel Back Up");
+
+                    //MachineAnimations.SetTrigger("Tape Wheel Front Up");
+                    //MachineAnimations.SetTrigger("Tape Wheel Back Up");
+                    // animatie doos door machine
+                }
+            }
+
+            if (collision.transform.CompareTag("LargeBox")  /* powerSwitch */)
+            {
+                Debug.Log("LargeBox Yes");
+                if (headHeight == 1f && alignment == 1f && !largeBoxTrigger)
+                {
+                    largeBoxTrigger = true;
+                    collision.gameObject.transform.SetParent(boneAnim.transform);
+                    MachineAnimations.SetTrigger("Doos door machine");
+
+                    MachineAnimations.SetTrigger("Tape Wheel Front Down");
+                    MachineAnimations.SetTrigger("Tape Wheel Back Down");
+                    StartCoroutine(WaitForBox());
+
+                    //MachineAnimations.SetTrigger("Tape Wheel Front Up");
+                    //MachineAnimations.SetTrigger("Tape Wheel Back Up");
                     // animatie doos door machine
                 }
             }
